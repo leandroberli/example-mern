544d8bb92391297a0acf89a6214bce6bce5a9815
leandroberli <leandroberli@gmail.com>
2019-10-25 00:21:20 -0300

    models & routes

--- example-mern/backend/server.js
+++ example-mern/backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -16,6 +16,12 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database is connected successfully")
 })
+
+const productsRouter = require('./routes/products');
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
